a5d844a8f99238e735ad8ecf3db1264bff540254
Leandro Mantovani <leomanto98@gmail.com>
2024-05-02 16:51:22 -0300

    Comments on geometric distribution and usage

--- Herramientas/statisticsUtilities.py
+++ Herramientas/statisticsUtilities.py
@@ -1,8 +1,10 @@
 SIMULATION_MAX_LIMIT = 10_000
 
 # Estimated probability and estimated number of times to get something
-# done until success. The simulation and its arguments are given by
-# func and args 
+# done until success with probability p (Geometric distribution mean). 
+# The simulation and its arguments are given by func and args.
+# Note that it is required that the simulation returns 1 if the value
+# satisfies the probability condition and 0 otherwise.
 def sim_statistics(func, args, N):
     acc_prob, acc_mean = 0, 0
     for _ in range(N):
