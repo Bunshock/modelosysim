1d880777dc802ed734df0c94dea971531bfb901f
Leandro Mantovani <leomanto98@gmail.com>
2024-05-12 18:55:30 -0300

    Added time efficiency measurements

--- Exercises/P4/p4_e4.py
+++ Exercises/P4/p4_e4.py
@@ -1,10 +1,12 @@
 from random import random
 import Tools.graph as graph
+from Tools.timeEfficiency import timeEfficiency
 
 # X takes values i in {1,...,10} with probabilities p[i]:
 p = [0.11, 0.14, 0.09, 0.08, 0.12, 0.10, 0.09, 0.07, 0.11, 0.09]
 
 N_SIM = 10_000
+N_TIME_CHECKS = 100
 
 # a) Aceptacion y Rechazo with uniform Y ~ U[1, 10]
 # c parameter is: 0.14 / 0.1 = 1.4
@@ -15,7 +17,14 @@ def AyR():
         if U <= p[Y-1] / (1.4 * 0.1):
             return Y
 
-x_vec = graph.generateDVA(N_SIM, AyR, *[])
+# Calculate time efficiency for this method
+time_AyR = 0
+for _ in range(N_TIME_CHECKS):
+    x_vec, time_elapsed = timeEfficiency(graph.generateDVA, *[N_SIM, AyR, *[]])
+    time_AyR += time_elapsed
+print(f'Time efficiency of AyR ({N_SIM} iterations): {(time_AyR / N_TIME_CHECKS * 1000):.4f} ms')
+
+# Graph only the last one generated
 graph.graphDVA(x_vec, N_SIM, title='Aceptacion y Rechazo')
 
 
@@ -28,7 +37,14 @@ def transformadaInversa():
         F += p[i-1]
     return i
 
-x_vec = graph.generateDVA(N_SIM, transformadaInversa, *[])
+# Calculate time efficiency for this method
+time_TI = 0
+for _ in range(N_TIME_CHECKS):
+    x_vec, time_elapsed = timeEfficiency(graph.generateDVA, *[N_SIM, transformadaInversa, *[]])
+    time_TI += time_elapsed
+print(f'Time efficiency of Transformada Inversa ({N_SIM} iterations): {(time_TI / N_TIME_CHECKS * 1000):.4f} ms')
+
+# Graph only the last one generated
 graph.graphDVA(x_vec, N_SIM, title='Transformada inversa')
 
 
@@ -41,6 +57,13 @@ for i in range(10):
 def metodoUrna():
     U = int(random() * 100)
     return urna[U]
-    
-x_vec = graph.generateDVA(N_SIM, metodoUrna, *[])
+
+# Calculate time efficiency for this method
+time_urna = 0
+for _ in range(N_TIME_CHECKS):
+    x_vec, time_elapsed = timeEfficiency(graph.generateDVA, *[N_SIM, metodoUrna, *[]])
+    time_urna += time_elapsed
+print(f'Time efficiency of urna ({N_SIM} iterations): {(time_urna / N_TIME_CHECKS * 1000):.4f} ms')
+
+# Graph only the last one generated
 graph.graphDVA(x_vec, N_SIM, title='Metodo de la urna')
