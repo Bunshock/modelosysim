410d4c42c02c064575c9b4761c74c6c941c4c962
Leandro Mantovani <leomanto98@gmail.com>
2024-05-13 11:40:12 -0300

    Solved c)

--- Exercises/P5/p5_e7.py
+++ Exercises/P5/p5_e7.py
@@ -5,6 +5,9 @@ import math
 def densidadX(x):
     return (1 <= x <= math.e) * (1 / x)
 
+
+
+# a) Desarrollar metodos para generar X
 """
 i) Transformada inversa: Integramos f de -inf a x
 buscando F(x):
@@ -42,9 +45,12 @@ def generarX_AyR():
         if U <= densidadX(Y) / (c * densidadX(Y)):
             return Y
 
+
 # b) Comparamos la eficiencia usando 10_000 simulaciones, 
 # comparando los promedios de los valores obtenidos en cada
 # metodo. Sabemos que E[X] = e - 1
+print('b)')
+
 N_SIM = 10_000
 
 sum_TI, sum_AyR = 0, 0
@@ -57,3 +63,20 @@ for _ in range(N_SIM):
 
 print(f'E[X] usando Transformada Inversa ({N_SIM} simulaciones): {sum_TI / N_SIM}')
 print(f'E[X] usando Aceptacion y Rechazo ({N_SIM} simulaciones): {sum_AyR / N_SIM}')
+
+
+# c) Calculamos P(X <= 2) usando ambos metodos
+print('c)')
+
+count_TI, count_AyR = 0, 0
+for _ in range(N_SIM):
+    X1 = generarX_TI()
+    if X1 <= 2:
+        count_TI += 1
+    
+    X2 = generarX_AyR()
+    if X2 <= 2:
+        count_AyR += 1
+
+print(f'Transformada Inversa ({N_SIM} simulaciones): P(X <= 2) = {count_TI / N_SIM}')
+print(f'Aceptacion y Rechazo ({N_SIM} simulaciones): P(X <= 2) = {count_AyR / N_SIM}')
